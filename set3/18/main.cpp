2392bcd9cb545e16e7b9e1b17c8402e8de9cfbdb
Nathan Lesman <147553061+nathanlesman@users.noreply.github.com>
2024-09-27 20:39:03 +0200

    Update main.cpp

--- set3/18/main.cpp
+++ set3/18/main.cpp
@@ -4,10 +4,12 @@ int main(int argc, char *argv[])
 {
     if (argc == 1) 
     {
-        cout << "Usage: " << argv[0] << " <argument-number> [args...]\n";
+        cout << "Usage: " << argv[0] << " <argument-number> [args...]\n"; //guide the user on the correct way to run the program with required arguments
         return 1;
     }
 
+
+    
     // Call structCall to check argument
     if (!structCall(argc, argv)) 
         return 1;
