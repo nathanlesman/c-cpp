dd37fb6656ba9e9b47d9a4fee631040ef216f4bd
Nathan Lesman <147553061+nathanlesman@users.noreply.github.com>
2024-09-27 19:42:10 +0200

    Update boundCall.cpp

--- set3/18/util/boundCall.cpp
+++ set3/18/util/boundCall.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 void boundCall(int argc, char *argv[]) {
-    size_t argNumber = std::atoi(argv[1]);
+    size_t argNumber = std::stoi(argv[1]);
     ReturnValues result = combine(argc, argv, argNumber);
 
     std::cout << "Argument number (bound call): " << result.nr << ", Value: " << result.value << "\n";
