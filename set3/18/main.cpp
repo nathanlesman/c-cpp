0ef783691126ebc0a34809fbbc9178cd849183d2
ulcii <ula8perovec@gmail.com>
2024-09-27 20:37:07 +0200

    Update main.cpp

--- set3/18/main.cpp
+++ set3/18/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     if (argc == 1) 
     {
         cout << "Usage: " << argv[0] << " <argument-number> [args...]\n";
-        return 0;
+        return 1;
     }
 
     // Call structCall to check argument
