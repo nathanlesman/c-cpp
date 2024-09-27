83c5988f09fc6cb512dba2947d5d2375a9e88f18
ulcii <ula8perovec@gmail.com>
2024-09-27 19:53:17 +0200

    Update boundCall.cpp

--- set3/18/util/boundCall.cpp
+++ set3/18/util/boundCall.cpp
@@ -1,11 +1,11 @@
 #include "util.ih"
 #include "../returnvalues/returnvalues.hh"
-#include <iostream>
 
-void boundCall(int argc, char *argv[]) {
-    size_t argNumber = std::stoi(argv[1]);
+void boundCall(size_t argc, char *argv[]) 
+{
+    size_t argNumber = stoul(argv[1]);
     ReturnValues result = combine(argc, argv, argNumber);
 
-    std::cout << "Argument number (bound call): " << result.nr << ", Value: " << result.value << "\n";
+    cout << "Argument number (bound call): " << result.nr << ", Value: " << result.value << "\n";
 }
 
