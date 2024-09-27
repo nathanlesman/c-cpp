9951d5c685df17f8b5324e6cbbdc1c9c830c0fc7
ulcii <ula8perovec@gmail.com>
2024-09-27 19:51:10 +0200

    Update util.hh

--- set3/18/util/util.hh
+++ set3/18/util/util.hh
@@ -1,8 +1,8 @@
 #ifndef UTIL_HH
 #define UTIL_HH
 
-bool structCall(int argc, char *argv[]);
-void boundCall(int argc, char *argv[]);
+bool structCall(size_t argc, char *argv[]);
+void boundCall(size_t argc, char *argv[]);
 
 #endif
 
