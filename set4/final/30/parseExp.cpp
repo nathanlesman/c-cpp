b8ab7d20743194393b97e066ca81b7349144e141
Nathan Lesman <147553061+nathanlesman@users.noreply.github.com>
2024-10-08 15:58:40 +0200

    Update parseExp.cpp

--- set4/final/30/parseExp.cpp
+++ set4/final/30/parseExp.cpp
@@ -1,9 +1,9 @@
 #include "calculator.ih"
 
-void parseExpression(string &line)
+void parseExpression()
 {
-    string str_lnumber = line.substr(0, d_first_space)
-    string str_rnumber = line.substr(d_second_space + 1)
+    string str_lnumber = d_line.substr(0, d_first_space)
+    string str_rnumber = d_line.substr(d_second_space + 1)
     
     d_lint = (str_lnumber.find(".") != string::npos)
     d_rint = (str_rnumber.find(".") != string::npos)
