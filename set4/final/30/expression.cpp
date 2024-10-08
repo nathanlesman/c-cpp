dca4eee8b1abe4f49041b3c7dfc56f50b25c0928
Nathan Lesman <147553061+nathanlesman@users.noreply.github.com>
2024-10-08 15:57:42 +0200

    Update expression.cpp

--- set4/final/30/expression.cpp
+++ set4/final/30/expression.cpp
@@ -2,20 +2,12 @@
 
 bool Calculator::expression(string &line)
 {
-    getSpaces(line)
+    getSpaces(line);
 
     if (d_first_space == string::npos || d_second_space == string::npos)
         return false;
 
-    string str_lnumber = line.substr(0, first_space)
-    string str_rnumber = line.substr(second_space + 1)
-    
-    d_lint = (str_lnumber.find(".") != string::npos)
-    d_rint = (str_rnumber.find(".") != string::npos)
-    
-    d_lnumber = stod(str_lnumber);
-    d_str_operation = line.substr(first_space + 1, second_space - first_space - 1);
-    d_rnumber = stod(str_rnumber);
+    parseExpression(line);
     
     if (getOperator(d_str_operation) && number(&d_lnumber, &d_lint) && number (&d_rnumber, &d_rint))
         return true;
