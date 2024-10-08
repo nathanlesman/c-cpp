5a182be97fbf6dad0f8ec34044ecd72533db167f
Nathan Lesman <147553061+nathanlesman@users.noreply.github.com>
2024-10-08 16:02:39 +0200

    Update expression.cpp

--- set4/final/30/expression.cpp
+++ set4/final/30/expression.cpp
@@ -1,13 +1,13 @@
 #include "calculator.ih"
 
-bool Calculator::expression(string &line)
+bool Calculator::expression()
 {
-    getSpaces(line);
+    getSpaces();
 
     if (d_first_space == string::npos || d_second_space == string::npos)
         return false;
 
-    parseExpression(line);
+    parseExpression();
     
     if (getOperator(d_str_operation) && number(&d_lnumber, &d_lint) && number (&d_rnumber, &d_rint))
         return true;
